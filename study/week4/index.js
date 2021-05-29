b70f9f19e10cefe2b9e664819d8cdbfe0e8a4e6c
ayberkbozkus <32310900+ayberkbozkus@users.noreply.github.com>
2021-05-29 19:03:39 +0300

    person add, delete, find app with get,post

--- study/week4/index.js
+++ study/week4/index.js
@@ -1,10 +1,40 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+const PersonService = require('./services/person-service')
+
+
 
 const app = express()
 
+app.use(bodyParser.json())
+app.set('view engine', 'pug')
+
 app.get('/', (req,res)=>{
     // res.send('Hello')
-    res.sendFile(__dirname + '/index.html')
+    // res.sendFile(__dirname + '/index.html')
+    res.render('index')
+})
+
+app.get('/person/all', async (req, res) => {
+    const people = await PersonService.findAll()
+    res.render('person', { people:people })
+  })
+
+app.get('/person/:id', async (req,res)=>{
+    const id = req.params.id
+    const person = await PersonService.find(id)
+    res.send(person)
+})
+
+app.post('/person', async (req,res)=>{
+    const person = await PersonService.add(req.body)
+    res.send(person)
+})
+
+app.delete('/person/:id', async (req,res)=>{
+    await PersonService.del(req.params.id)
+    res.send('ok')
 })
 
 app.listen(3000, ()=> {
